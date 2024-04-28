2aae678addce695cec6003f627aa9480aa7e934a
Ladamenko <ladamenko@gmail.com>
2024-04-28 21:50:25 +0300

    first task done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import Profile from '../Profile/Profile';
+import userData from '../../userData.json';
+
+export default function App() {
+  return (
+    <>
+      <Profile userData={userData} />
+    </>
+  );
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,39 @@
+// import clsx from 'clsx';
+import css from './Profile.module.css';
+import './Profile.module.css';
+
+export default function Profile({
+  userData: {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  },
+}) {
+  return (
+    <div className={css.container}>
+      <div>
+        <img className={css.avatar} src={avatar} alt="User avatar" />
+        <p className={css.username}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.list}>
+        <li className={css.item}>
+          <span>Followers</span>
+          <span className={css.value}>{followers}</span>
+        </li>
+        <li className={css.item}>
+          <span>Views</span>
+          <span className={css.value}>{views}</span>
+        </li>
+        <li className={css.item}>
+          <span>Likes</span>
+          <span className={css.value}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
